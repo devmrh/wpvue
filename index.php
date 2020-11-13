83138e0524b35b989a85263c8c889ab38ace8c5a
mohammadreza <dev.mohammadreza@yahoo.com>
2020-11-13 17:01:25 +0330

    add filter query

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ use App\Model\Property;
 use App\Model\PropertyCategory;
 use App\Model\Province;
 use App\Model\SellType;
+use Illuminate\Database\Capsule\Manager as DB;
 
 require_once("vendor/autoload.php");
 Database::init();
@@ -74,6 +75,8 @@ add_action( 'rest_api_init', function () {
   register_rest_route( 'api/v1', '/data/get-cities', array(
         'methods' => 'GET',
         'callback' => 'ea_get_cities',
+        'permissions_callback' => 'is_user_logged_in',
+
       ) );
   } );
 
@@ -91,6 +94,8 @@ add_action( 'rest_api_init', function () {
   register_rest_route( 'api/v1', '/data/', array(
         'methods' => 'GET',
         'callback' => 'ea_get_data',
+        'permissions_callback' => 'is_user_logged_in',
+
       ) );
   } );
 
@@ -117,17 +122,32 @@ add_action( 'rest_api_init', function () {
   }
 
 
+
+
 // save data.
 add_action( 'rest_api_init', function () {
   register_rest_route( 'api/v1', '/data/save-form', array(
         'methods' => 'POST',
         'callback' => 'ea_save_form',
+        'permissions_callback' => 'is_user_logged_in',
       ) );
   } );
 
   function ea_save_form( $data ) {
       $params = $data->get_params();
      // return $params['build_time'];
+
+      if(empty($params['title'])){
+        return wp_send_json_error("عنوان نمیتواند خالی باشد", 422);
+      }
+      if(empty($params['city_id'])){
+        return wp_send_json_error("شهر نمیتواند خالی باشد", 422);
+      }
+      if(empty($params['province_id'])){
+        return wp_send_json_error("استان نمیتواند خالی باشد", 422);
+      }
+
+
       $property = Property::create([
         'title'=> $params['title'],
         'property_category_id'=> $params['property_category_id'] ?? null,
@@ -161,15 +181,27 @@ add_action( 'rest_api_init', function () {
 
 // edit data.
 add_action( 'rest_api_init', function () {
-  register_rest_route( 'api/v1', '/data/save-edit-form', array(
+  register_rest_route( 'api/v1', '/data/update-form', array(
         'methods' => 'POST',
         'callback' => 'ea_edit_form',
+        'permissions_callback' => 'is_user_logged_in',
+
       ) );
   } );
 
   function ea_edit_form( $data ) {
       $params = $data->get_params();
       $form_id = $params['id'];
+
+      if(empty($params['title'])){
+        return wp_send_json_error("عنوان نمیتواند خالی باشد", 422);
+      }
+      if(empty($params['city_id'])){
+        return wp_send_json_error("شهر نمیتواند خالی باشد", 422);
+      }
+      if(empty($params['province_id'])){
+        return wp_send_json_error("استان نمیتواند خالی باشد", 422);
+      }
 
       Property::find($form_id)->update([
         'title'=> $params['title'],
@@ -180,8 +212,8 @@ add_action( 'rest_api_init', function () {
         'price_label'=> $params['price_label'] ?? '',
         'rent_price'=> $params['rent_price'] ?? '',
         'rent_label'=> $params['rent_label'] ?? '',
-        'province_id'=> $params['province_id'] ?? '',
-        'city_id'=> $params['city_id'] ?? '',
+        'province_id'=> $params['province_id'] ?? null,
+        'city_id'=> $params['city_id'] ?? null,
         'size'=> $params['size'] ?? '',
         'size_unit'=> $params['size_unit'] ?? '',
         'bed_count'=> $params['bed_count'] ?? '',
@@ -207,6 +239,8 @@ add_action( 'rest_api_init', function () {
   register_rest_route( 'api/v1', '/data/get-all-properties', array(
         'methods' => 'GET',
         'callback' => 'ea_get_properties',
+        'permissions_callback' => 'is_user_logged_in',
+
       ) );
   } );
 
@@ -249,11 +283,13 @@ function ea_get_properties( $data ) {
 }
 
 
-// save data.
+// delete property.
 add_action( 'rest_api_init', function () {
   register_rest_route( 'api/v1', '/data/delete-property', array(
         'methods' => 'POST',
         'callback' => 'ea_delete_form',
+        'permissions_callback' => 'is_user_logged_in',
+
       ) );
   } );
 
@@ -265,6 +301,157 @@ add_action( 'rest_api_init', function () {
 
 }
 
+
+// search form
+
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'api/v1', '/data/search', array(
+        'methods' => 'GET',
+        'callback' => 'ea_get_data_search',
+       // 'permissions_callback' => 'is_user_logged_in',
+
+      ) );
+  } );
+
+  function ea_get_data_search( $data ) {
+    $params = $data->get_params();
+
+
+    $from_build_time = (int)$params['from_build_time'];
+    $to_build_time = (int)$params['to_build_time'];
+    $from_price = (int)$params['from_price'];
+    $to_price = (int)$params['to_price'];
+    $from_rent_price = (int)$params['from_rent_price'];
+    $to_rent_price = (int)$params['to_rent_price'];
+    $from_size = (int)$params['from_size'];
+    $to_size =  (int)$params['to_size'];
+    $bed_count = $params['bed_count'];
+    $bath_count = $params['bath_count'];
+    $parking_count = $params['parking_count'];
+    $owner = $params['owner'];
+    $special = $params['special'];
+    $address = $params['address'];
+    $property_category_id = $params['property_category_id'];
+    $sell_type_id = $params['sell_type_id'];
+    $feature_id = $params['feature_id'];
+    $city_id = $params['city_id'];
+    $province_id = $params['province_id'];
+
+    $facilities = $params['facilities']; // array
+    $sellTypes = $params['sellTypes'] ?? []; // array
+
+
+    //return $facilities;
+    $query = Property::query();
+    // We search value in multiple columns.
+
+    if( is_array($facilities) && count($facilities) > 0){
+     // eval("\$facilities = $facilities;");
+
+    }else{
+      $facilities = [];
+
+    }
+    // $list = '('.implode(', ', $facilities).')';
+
+    // return $list;
+   // return $facilities;
+
+
+    $query->when($owner, function ($q, $search) {
+      return $q->where('owner', 'like', '%'.$search.'%' );
+    });
+    $query->when($bed_count, function ($q, $search) {
+      return $q->where('bed_count', $search );
+    });
+    $query->when($bath_count, function ($q, $search) {
+      return $q->where('bath_count', $search );
+    });
+    $query->when($parking_count, function ($q, $search) {
+      return $q->where('parking_count', $search );
+    });
+    $query->when($special, function ($q, $search) {
+      return $q->where('special', 1 );
+    });
+    $query->when($address, function ($q, $search) {
+      return $q->where('address', 'like', '%'.$search.'%' );
+
+    });
+    $query->when($property_category_id, function ($q, $search) {
+      return $q->where('property_category_id', $search );
+
+    });
+    $query->when($sell_type_id, function ($q, $search) {
+      return $q->where('sell_type_id', $search );
+
+    });
+    $query->when($feature_id, function ($q, $search) {
+      return $q->where('feature_id', $search );
+
+    });
+    $query->when($city_id, function ($q, $search) {
+      return $q->where('city_id', $search );
+
+    });
+    $query->when($province_id, function ($q, $search) {
+      return $q->where('province_id', $search );
+
+    });
+
+    $query->when($sellTypes, function ($q, $search) {
+      return  $q->whereIn('sell_type_id', $search);
+
+    });
+
+
+    if($from_build_time  || $to_build_time ){
+      if(!$to_build_time){
+        $to_build_time = 100000000000;
+      }
+      $query->whereRaw("(build_time <= ? AND build_time >= ?) ", [$to_build_time, $from_build_time]);
+    }
+    if($from_price  || $to_price ){
+      if(!$to_price){
+        $to_price = 100000000000;
+      }
+      $query->whereRaw("(price <= ? AND price >= ?) ", [$to_price, $from_price]);
+    }
+    if($from_rent_price  || $to_rent_price ){
+      if(!$to_rent_price){
+        $to_rent_price = 100000000;
+      }
+      $query->whereRaw("(rent_price <= ? AND rent_price >= ?) ", [$to_rent_price, $from_rent_price]);
+    }
+
+    if($from_size  || $to_size ){
+      if(!$to_size){
+        $to_size = 1000;
+      }
+      $query->whereRaw("(size <= ? AND size >= ?) ", [$to_size  , $from_size ]);
+    }
+
+   // $data = [];
+    $data =  $query->limit(50)->get()->toArray();
+   // return $data;
+
+  if($facilities){
+      $list = '('.implode(', ', $facilities).')';
+      $re = DB::select("SELECT DISTINCT *
+      FROM properties pro JOIN
+      facility_property gc
+            ON pro.id = gc.property_id JOIN
+            facilities c
+            ON c.id = gc.facility_id
+      WHERE c.id IN $list");
+    //  return gettype($data);
+      $bb = array_merge($re, $data);
+      return $bb;
+    }
+    return $data;
+
+
+
+}
 
 
 ?>
